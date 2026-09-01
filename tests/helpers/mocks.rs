12aefbfb12a51fe9a012462f54dfeac24c1e9c05
vnrg <volodya-nrg@mail.ru>
2026-09-01 19:16:20 +0300

    save

--- tests/helpers/mocks.rs
+++ tests/helpers/mocks.rs
@@ -1,9 +1,9 @@
-use mkk_basis::adapter::email::EmailSender;
+use mkk_basis::adapter::email::{EmailError, EmailSender};
 
 #[derive(Clone)]
 pub struct EmailServiceMock {}
 impl EmailSender for EmailServiceMock {
-    fn send(&self, _to: String, _subject: String, _body: String) -> Result<(), String> {
+    fn send(&self, _to: String, _subject: String, _body: String) -> Result<(), EmailError> {
         Ok(())
     }
 }
